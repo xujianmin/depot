40d8d0949e4033eff29c609992e61e7eb5954a82
Jimmy Xu <jianmin.xu@usingnow.com>
2024-10-30 16:26:53 +0800

    Iteration B2.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true
 
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif/jpg/png)\z}i,
+    with: %r{\.(gif|jpg|png)\z}i,
     message: "must be a URL for GIF, JPG or PNG image."
   }
 end
