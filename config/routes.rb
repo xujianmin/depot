4cc8b5c27db3299fed1e769212923c9a7b2c00ec
Jimmy Xu <jianmin.xu@usingnow.com>
2024-10-31 23:39:48 +0800

    Iteration: C.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get "store/index"
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "store#index", as: "store_index"
 end
