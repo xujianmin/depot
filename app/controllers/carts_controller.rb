68573d23f031ab95be8f2d921b5f2df5de80e2a5
Jimmy Xu <jianmin.xu@usingnow.com>
2024-11-11 23:31:39 +0800

    Iteraton: E2. Rescue error for invalid cart.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[ show edit update destroy ]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+
   # GET /carts or /carts.json
   def index
     @carts = Cart.all
@@ -66,5 +68,10 @@ class CartsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def cart_params
       params.fetch(:cart, {})
+    end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: "Invalid cart"
     end
 end
