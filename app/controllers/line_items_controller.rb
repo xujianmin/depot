45367c97d7ccc0d0cc74d4b3ad71c253866c59f4
Jimmy Xu <jianmin.xu@usingnow.com>
2024-11-12 14:33:47 +0800

    Iteration: F1/F2.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart) }
+        format.turbo_stream
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
