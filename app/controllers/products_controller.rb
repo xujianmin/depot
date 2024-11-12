494a068425d092acffb243ce7741fd91d07864c5
Jimmy Xu <jianmin.xu@usingnow.com>
2024-11-12 16:02:44 +0800

    Iteration: F4.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,7 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to @product, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+        @product.broadcast_replace_later_to "products", partial: "store/product"
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
